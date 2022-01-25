e38e3468c8af08c946efff6febefc09108aad4af
kenriortega <kenriortega@gmail.com>
2022-01-24 21:40:24 -0500

    ref: sum and sub operations

--- script.exs
+++ script.exs
@@ -1,8 +1,25 @@
 defmodule Mates do
   def sum(x, y) do
-    x + y
+    operation({:sum, x, y})
+  end
+
+  def sub(x, y) do
+    operation({:sub, x, y})
+  end
+
+  # private fn
+  defp operation(params) do
+    {op, x, y} = params
+
+    if op == :sum do
+      x + y
+    else
+      x - y
+    end
   end
 end
 
 result = Mates.sum(2, 3)
+IO.puts("result is #{result}")
+result = Mates.sub(4, 3)
 IO.puts("result is #{result}")
