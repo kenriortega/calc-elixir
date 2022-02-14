2662d89caf16dd33f47d2bb50a133ae42eaf7e09
kenriortega <kenriortega@gmail.com>
2022-02-13 19:26:41 -0500

    add docs

--- calculator/lib/calc.ex
+++ calculator/lib/calc.ex
@@ -1,11 +1,29 @@
 defmodule Calc do
+  @moduledoc """
+  A basic calc module
+
+  You can be use std/elixir for this operations
+
+  ## Example
+  iex> Calc.sum(25,11)
+  36
+  """
   alias Calc.Operation
 
-  # public fn
+  @doc """
+  Add `x` and `y` and return the result of the sum
+  """
   def sum(x, y) do
     Calc.Private.operation(%Operation{a: x, b: y, operation: &+/2})
   end
 
+  @doc """
+  Add `x` and `y` and return the result of the sub
+
+  ## Examples
+  iex> Calc.sub(5,1)
+  4
+  """
   def sub(x, y) do
     Calc.Private.operation(%Operation{a: x, b: y, operation: &-/2})
   end
